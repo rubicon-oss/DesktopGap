a2d815c370387af7f0e1729c56ae583f0a0242e2
unknown <claus.matzinger@W0384.int.rubicon-it.com>
2013-03-11 14:59:56 +0100

    - created and configured projects - initial commit

---
+++ Clients.Windows/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿// This file is part of DesktopGap (desktopgap.codeplex.com)
+// Copyright (c) rubicon IT GmbH, www.rubicon.eu
+// 
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+//
+// Additional permissions are listed in the file DesktopGap_exceptions.txt.
+// 
+using System;
+using System.Windows;
+
+namespace DesktopGap.GUI
+{
+  /// <summary>
+  /// Interaction logic for MainWindow.xaml
+  /// </summary>
+  public partial class MainWindow : Window
+  {
+    public MainWindow ()
+    {
+      InitializeComponent();
+    }
+  }
+}
